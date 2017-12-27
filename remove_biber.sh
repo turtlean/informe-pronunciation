fcc27b025bc53701295c98897346c924a81d9ba2
Leandro Matayoshi <leandro.matayoshi@gmail.com>
2017-12-27 02:23:24 -0300

    Agregado previous work en introducción

---
+++ remove_biber.sh
@@ -0,0 +1,12 @@
+rm informe.bbl
+rm informe.bcf
+rm informe.blg
+rm informe.fdb_latexmk
+rm informe.fls
+rm informe.log
+rm informe.out
+rm informe.run.xml
+rm informe.synctex.gz
+rm informe.toc
+rm informe.pdf
+rm informe.aux
